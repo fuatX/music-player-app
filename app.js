7ca5a761447bf7c970d8942d9e982214345e5d36
fuatX <fuatyagizaydin@hotmail.com>
2023-08-10 23:14:01 +0300

    music-app-started

---
+++ app.js
@@ -0,0 +1,5 @@
+const player = new MusicPlayer(musicList);
+
+let music = player.getMusic(); //const yapınca değiştirilemediği için hata veriyor.
+
+console.log(music.getName());
---
+++ music.js
@@ -0,0 +1,22 @@
+class Music {
+  constructor(title, singer, img, file) {
+    this.title = title;
+    this.singer = singer;
+    this.img = img;
+    this.file = file;
+  }
+
+  getName() {
+    return this.title + " - " + this.singer;
+  }
+}
+
+const musicList = [
+  new Music("FMA-OP", "Again", "1.jpg", "1.mp3"),
+  new Music("Olduramadım", "Özkan Uğur", "2.jpg", "2.mp3"),
+  new Music("Zaferlerim", "Demir Demirkıran", "3.jpg", "3.mp3"),
+];
+
+// for (music of musicList) {
+//   console.log(music.getName());
+// }
